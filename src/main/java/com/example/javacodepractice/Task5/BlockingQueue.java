ee38509447cb15d004a82df9f9d1d3c1461acf72
VadiMayer <h4998998482@gmail.com>
2024-05-14 18:08:14 +0300

    etiting BlockingQueue

--- src/main/java/com/example/javacodepractice/Task5/BlockingQueue.java
+++ src/main/java/com/example/javacodepractice/Task5/BlockingQueue.java
@@ -1,43 +1,60 @@
 package com.example.javacodepractice.Task5;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class BlockingQueue<E> {
 
-    private LinkedList<E> storage;
+    private Queue<E> storage;
 
     private final int size;
 
     private AtomicInteger currentSize;
 
+    private final Object lock;
+
     public BlockingQueue(int size) {
         this.size = size;
         storage = new LinkedList<>();
+        lock = new Object();
     }
 
     //enqueue() для добавления элемента в очередь
-    boolean enqueue(E e) throws InterruptedException {
-        if (currentSize.get() == size) {
-            Thread.currentThread().wait();
-            return false;
+    public boolean enqueue(E e) throws InterruptedException {
+        synchronized (lock) {
+            if (currentSize.get() == size) {
+                lock.wait();
+                return false;
+            } else {
+                storage.add(e);
+                currentSize.incrementAndGet();
+                lock.notify();
+                return true;
+            }
         }
-        storage.add(currentSize.get(), e);
-        currentSize.incrementAndGet();
-        return true;
     }
 
     // dequeue() для извлечения элемента
     // Если очередь пуста, dequeue() должен блокировать вызывающий поток до появления нового элемента.
-    E dequeue() throws InterruptedException {
-        if (currentSize.get() == 0) {
-            Thread.currentThread().wait();
-            return null;
-        } else
-            return storage.pollFirst();
+    public E dequeue() throws InterruptedException {
+        synchronized (lock) {
+            if (currentSize.get() == 0) {
+                lock.wait();
+                return null;
+            } else {
+                E firstElement = storage.remove();
+                lock.notify();
+                return firstElement;
+            }
+        }
     }
 
     public int size() {
         return currentSize.get();
+    }
+
+    public void getElements() {
+        System.out.println(storage);
     }
 }
