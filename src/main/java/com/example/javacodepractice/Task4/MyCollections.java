efb557cbd423b45b857437907345c9f0d446fbe0
VadiMayer <h4998998482@gmail.com>
2024-05-16 14:04:22 +0300

    editing the task4

--- src/main/java/com/example/javacodepractice/Task4/MyCollections.java
+++ src/main/java/com/example/javacodepractice/Task4/MyCollections.java
@@ -1,33 +1,17 @@
 package com.example.javacodepractice.Task4;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 class MyCollections {
     <K> Map<K, Integer> countOfElements(K[] array) {
 
         Map<K, Integer> mapForCountElements = new HashMap<>();
 
-        int index = 0;
-
-        Set<Map.Entry<K, Integer>> entrySet = mapForCountElements.entrySet();
-
-        for (int i = 0; i < array.length; i++) {
-
-            mapForCountElements.putIfAbsent(array[i], 0);
-            Iterator<Map.Entry<K, Integer>> iterator = entrySet.iterator();
-
-            for (int j = index; j < entrySet.size(); j++) {
-                Map.Entry<K, Integer> entry = iterator.next();
-                if (mapForCountElements.containsKey(array[i])) {
-                    entry.setValue(entry.getValue() + 1);
-                    index++;
-                    break;
-                }
-            }
+        for (K element : array) {
+            mapForCountElements.put(element, mapForCountElements.getOrDefault(element, 0) + 1);
         }
+
         return mapForCountElements;
     }
 }
--- src/test/java/com/example/javacodepractice/Task4/MyCollectionsTest.java
+++ src/test/java/com/example/javacodepractice/Task4/MyCollectionsTest.java
@@ -10,11 +10,22 @@ class MyCollectionsTest {
     @Test
     void countOfElementsInteger() {
         MyCollections myCollections = new MyCollections();
-        Integer[] ints = new Integer[10];
+        Integer[] ints = new Integer[6000];
         for (int i = 0; i < ints.length; i++) {
-            ints[i] = i;
+            ints[i] = (int) (Math.random() * 10) + 1;
         }
         Map<Integer, Integer> map = myCollections.countOfElements(ints);
+        System.out.println(map);
+    }
+
+    @Test
+    void countOfElementsStrings() {
+        MyCollections myCollections = new MyCollections();
+        String[] strings = new String[5000];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = (int) (Math.random() * 10) + 1 + " ";
+        }
+        Map<String, Integer> map = myCollections.countOfElements(strings);
         System.out.println(map);
     }
 }
