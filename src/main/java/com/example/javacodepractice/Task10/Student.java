0e07ffb47b1cf9518693fd2ad0339584b2501f0f
VadiMayer <h4998998482@gmail.com>
2024-06-04 16:57:43 +0300

    completing the task10

---
+++ src/main/java/com/example/javacodepractice/Task10/Student.java
@@ -0,0 +1,22 @@
+package com.example.javacodepractice.Task10;
+
+import java.util.Map;
+
+public class Student {
+
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
